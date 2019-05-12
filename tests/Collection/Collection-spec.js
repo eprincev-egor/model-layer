aaec446354d8ba10a5b4ae4981a327ac726ff490
eprincev.egor@yandex.ru <eprincev.egor@yandex.ru>
2019-05-12 12:16:58 +0300

    +indexOf +includes

--- tests/Collection/Collection-spec.js
+++ tests/Collection/Collection-spec.js
@@ -771,5 +771,137 @@ describe("Collection tests", () => {
         assert.strictEqual( result.length, 1 );
         assert.strictEqual( result[0].get("name"), "Pie" );
     });
+
+    it("indexOf(model)", () => {
+
+        class Products extends Collection {
+            static structure() {
+                return {
+                    name: "text",
+                    price: "number"
+                };
+            }
+        }
+
+        let products = new Products([
+            {name: "Eggs", price: 1.8},
+            {name: "Pie", price: 10},
+            {name: "Milk", price: 4}
+        ]);
+
+        let firstModel = products.at(0);
+        let lastModel = products.at(2);
+
+        class SomeModel {
+            static structure() {
+                return {"*": "*"};
+            }
+        }
+        let unknownModel = new SomeModel();
+
+        
+        let result = products.indexOf( lastModel );
+        assert.strictEqual( result, 2 );
+
+        result = products.indexOf( firstModel );
+        assert.strictEqual( result, 0 );
+
+        result = products.indexOf( unknownModel );
+        assert.strictEqual( result, -1 );
+    });
     
+    it("indexOf(model, fromIndex)", () => {
+
+        class Products extends Collection {
+            static structure() {
+                return {
+                    name: "text",
+                    price: "number"
+                };
+            }
+        }
+
+        let products = new Products([
+            {name: "Eggs", price: 1.8},
+            {name: "Pie", price: 10},
+            {name: "Milk", price: 4}
+        ]);
+
+        let firstModel = products.at(0);
+        let lastModel = products.at(2);
+
+        
+        let result = products.indexOf( lastModel, 1 );
+        assert.strictEqual( result, 2 );
+
+        result = products.indexOf( firstModel, 1 );
+        assert.strictEqual( result, -1 );
+    });
+
+
+    it("includes(model)", () => {
+
+        class Products extends Collection {
+            static structure() {
+                return {
+                    name: "text",
+                    price: "number"
+                };
+            }
+        }
+
+        let products = new Products([
+            {name: "Eggs", price: 1.8},
+            {name: "Pie", price: 10},
+            {name: "Milk", price: 4}
+        ]);
+
+        let firstModel = products.at(0);
+        let lastModel = products.at(2);
+
+        class SomeModel {
+            static structure() {
+                return {"*": "*"};
+            }
+        }
+        let unknownModel = new SomeModel();
+
+        
+        let result = products.includes( lastModel );
+        assert.strictEqual( result, true );
+
+        result = products.includes( firstModel );
+        assert.strictEqual( result, true );
+
+        result = products.includes( unknownModel );
+        assert.strictEqual( result, false );
+    });
+
+    it("includes(model, fromIndex)", () => {
+
+        class Products extends Collection {
+            static structure() {
+                return {
+                    name: "text",
+                    price: "number"
+                };
+            }
+        }
+
+        let products = new Products([
+            {name: "Eggs", price: 1.8},
+            {name: "Pie", price: 10},
+            {name: "Milk", price: 4}
+        ]);
+
+        let firstModel = products.at(0);
+        let lastModel = products.at(2);
+
+        
+        let result = products.includes( lastModel, 1 );
+        assert.strictEqual( result, true );
+
+        result = products.includes( firstModel, 1 );
+        assert.strictEqual( result, false );
+    });
 });
