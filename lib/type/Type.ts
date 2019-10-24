d47c8ad5ddc89165ca9af3beb5031d4a5e783fc3
eprincev.egor <eprincev.egor@yandex.ru>
2019-10-24 20:23:22 +0300

    tslint

--- lib/type/Type.ts
+++ lib/type/Type.ts
@@ -11,6 +11,85 @@ const FORBIDDEN_PRIMARY_KEYS = [
 export default class Type {
     public static Model: any;
 
+    public static registerType(typeName, CustomType) {
+        registeredTypes[ typeName ] = CustomType;
+    }
+
+    public static getType(typeName) {
+        return registeredTypes[ typeName ];
+    }
+
+    // create type by params
+    public static create(description, key) {
+        
+        const isPlainDescription = (
+            description && 
+            description.type
+        );
+
+        // structure: {prop: "number"}
+        // or
+        // structure: {prop: ["number"]}
+        // or
+        // structure: {prop: SomeModel}
+        // or
+        // structure: {prop: {}}
+        if ( !isPlainDescription ) {
+            description = {
+                type: description
+            };
+        }
+        
+
+        
+
+        // prepare description: ["string"] 
+        // to { type: "array", element: {type: "string"} }
+        for (const key in registeredTypes) {
+            const CustomType = registeredTypes[ key ];
+
+            // CustomType can use some variations for declare structure
+            CustomType.prepareDescription( description );
+        }
+
+        // find CustomType by name
+        const CustomType = registeredTypes[ description.type ];
+        if ( !CustomType ) {
+            throw new Error(`${key}: unknown type: ${ description.type }`);
+        }
+
+
+        if ( description.primary ) {
+            const Model = Type.Model;
+            const isReserved = (
+                key in Model.prototype ||
+                FORBIDDEN_PRIMARY_KEYS.includes( key )
+            );
+
+            if ( isReserved ) {
+                throw new Error(`primary key cannot be reserved word: ${ key }`);
+            }
+        }
+
+        try {
+            description = new CustomType( description );
+        } catch (err) {
+            err.message = key + ": " + err.message;
+            throw err;
+        }
+
+        // structure must be static
+        Object.freeze( description );
+
+        return description;
+    }
+
+    // default behavior
+    public static prepareDescription(description) {
+        // redefine me
+        return description;
+    }
+
     constructor({
         type,
         required = false,
@@ -43,8 +122,8 @@ export default class Type {
 
         // custom prepare not null value, after default prepare
         if ( typeof prepare == "function" ) {
-            let prepareByType = this.prepare.bind(this);
-            let customPrepare = prepare;
+            const prepareByType = this.prepare.bind(this);
+            const customPrepare = prepare;
 
             this.prepare = (value) => {
                 value = prepareByType( value );
@@ -65,9 +144,9 @@ export default class Type {
         // custom validate by RegExp or function
         if ( validate ) {
             // validate by required, enum, "unique" (ArrayType)
-            let validateByType = this.validate.bind(this);
+            const validateByType = this.validate.bind(this);
             // validate by RegExp or function
-            let customValidate = validate;
+            const customValidate = validate;
 
             if ( typeof validate == "function" ) {
                 this.validate = (value, key) => {
@@ -101,7 +180,7 @@ export default class Type {
 
         // validate key for models with "*"
         if ( key ) {
-            let customValidateKey = key;
+            const customValidateKey = key;
 
             if ( customValidateKey instanceof RegExp ) {
                 this.validateKey = (key) => {
@@ -124,15 +203,15 @@ export default class Type {
         }
     }
 
-    default() {
+    public default() {
         return null;
     }
 
-    validateKey(/* key */) {
+    public validateKey(/* key */) {
         return true;
     }
 
-    validate(value) {
+    public validate(value) {
         if ( this.enum ) {
             if ( value != null ) {
                 return this.enum.includes( value );
@@ -142,102 +221,23 @@ export default class Type {
         return true;
     }
 
-    prepare(value) {
+    public prepare(value) {
         return value;
     }
 
-    toJSON(value) {
+    public toJSON(value) {
         return value;
     }
 
-    clone(value) {
+    public clone(value) {
         return this.toJSON( value );
     }
 
-    typeAsString() {
+    public typeAsString() {
         return this.type;
     }
 
-    equal(selfValue, otherValue /*, stack */) {
+    public equal(selfValue, otherValue /*, stack */) {
         return selfValue === otherValue;
-    }
-
-    static registerType(typeName, CustomType) {
-        registeredTypes[ typeName ] = CustomType;
-    }
-
-    static getType(typeName) {
-        return registeredTypes[ typeName ];
-    }
-
-    // create type by params
-    static create(description, key) {
-        
-        let isPlainDescription = (
-            description && 
-            description.type
-        );
-
-        // structure: {prop: "number"}
-        // or
-        // structure: {prop: ["number"]}
-        // or
-        // structure: {prop: SomeModel}
-        // or
-        // structure: {prop: {}}
-        if ( !isPlainDescription ) {
-            description = {
-                type: description
-            };
-        }
-        
-
-        
-
-        // prepare description: ["string"] 
-        // to { type: "array", element: {type: "string"} }
-        for (let key in registeredTypes) {
-            let CustomType = registeredTypes[ key ];
-
-            // CustomType can use some variations for declare structure
-            CustomType.prepareDescription( description );
-        }
-
-        // find CustomType by name
-        let CustomType = registeredTypes[ description.type ];
-        if ( !CustomType ) {
-            throw new Error(`${key}: unknown type: ${ description.type }`);
-        }
-
-
-        if ( description.primary ) {
-            const Model = Type.Model;
-            const isReserved = (
-                key in Model.prototype ||
-                FORBIDDEN_PRIMARY_KEYS.includes( key )
-            );
-
-            if ( isReserved ) {
-                throw new Error(`primary key cannot be reserved word: ${ key }`);
-            }
-        }
-
-        try {
-            description = new CustomType( description );
-        } catch(err) {
-            err.message = key + ": " + err.message;
-            throw err;
-        }
-
-        // structure must be static
-        Object.freeze( description );
-
-        return description;
-    }
-
-    // default behavior
-    static prepareDescription(description) {
-        // redefine me
-        return description;
     }
 }
