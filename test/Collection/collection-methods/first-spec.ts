d7c5278e7f3c02173bc0b796419337653d030311
eprincev.egor@yandex.ru <eprincev.egor@yandex.ru>
2019-10-26 21:11:35 +0300

    +first

--- test/Collection/collection-methods/first-spec.ts
+++ test/Collection/collection-methods/first-spec.ts
@@ -1,14 +1,19 @@
 
+import {Collection, Model} from "../../../lib/index";
+import assert from "assert";
 
-const {Collection} = require("../../../lib/index");
-const assert = require("assert");
+interface IProduct {
+    name: string;
+    price: number;
+}
+class Product extends Model<IProduct> {}
 
 describe("Collection.first", () => {
 
     it("first()", () => {
         
-        class Products extends Collection {
-            static data() {
+        class Products extends Collection<Product> {
+            public static data() {
                 return {
                     name: "text",
                     price: "number"
@@ -16,13 +21,13 @@ describe("Collection.first", () => {
             }
         }
 
-        let products = new Products([
+        const products = new Products([
             {name: "Eggs", price: 1.8},
             {name: "Pie", price: 10}
         ]);
 
 
-        let model = products.first();
+        const model = products.first();
 
         assert.strictEqual( products.at(0), model );
 
